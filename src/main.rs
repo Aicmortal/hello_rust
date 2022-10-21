436f72acc75757e85ac33fc228c14086dce007aa
Aicmortal <aicmortal@gmail.com>
2022-10-21 17:50:08 +0800

    learn ownership

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+
+    let lens = calc_length(&s);
+    println!("{s}");
+    println!("{lens}");
+    change(&mut s);
+    let lens = calc_length(&s);
+    println!("{s}");
+    println!("{lens}");
+}
+
+fn calc_length(s: &String) -> usize {
+    s.len()
+}
+
+/// # chang
+fn change(some_string: &mut String) {
+    *some_string = String::from("123");
 }
